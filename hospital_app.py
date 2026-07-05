7402cae544d99a4ede21cebfe89d8b4f5469928c
dreageaneinjuliantoteddy-hash <dreageaneinjuliantoteddy@gmail.com>
2026-07-05 14:20:30 +0700

    update

--- hospital_app.py
+++ hospital_app.py
@@ -166,7 +166,7 @@ with st.form("triage_form"):
     <div style="background:#0921f6;border:1px solid #0d0ddc;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#0a2df2;color:rgb(255, 255, 255);border-radius:8px;
+            <span style="background:#0adff2;color:rgb(255, 255, 255);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">2</span>
             <span style="font-size:16px;font-weight:600;color:#000000;">How long have you had these symtomps ? </span>
         </div>
